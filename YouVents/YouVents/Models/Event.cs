050ead9a34e366f7d1e795efb821e1623e8a5615
TravisBubb <travisbubb14@gmail.com>
2022-02-23 14:25:35 -0500

    Create models for Venue and Event objects

---
+++ YouVents/YouVents/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YouVents.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+}
---
+++ YouVents/YouVents/Models/Venue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YouVents.Models
+{
+    public class Venue
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
