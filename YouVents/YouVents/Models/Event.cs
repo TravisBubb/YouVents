2f6ebb6b888209d2dea755c1318d46e4e2d3d876
TravisBubb <travisbubb14@gmail.com>
2022-02-23 20:30:01 -0500

    Update Event model to include zip and price

--- YouVents/YouVents/Models/Event.cs
+++ YouVents/YouVents/Models/Event.cs
@@ -8,10 +8,17 @@ namespace YouVents.Models
     public class Event
     {
         public int Id { get; set; }
+        public string OrganizerId { get; set; }
         public string Name { get; set; }
         public int Rating { get; set; }
         public string Description { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
+        public int Capacity { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public float Price { get; set; }
     }
 }
