2c3bcbef17191d6c70b4e54b60b79b7a8e63a8d5
TravisBubb <travisbubb14@gmail.com>
2022-04-02 13:25:11 -0400

    Removed queries from the back of page; call API instead

--- YouVents/YouVents/Pages/Events/Checkout.cshtml.cs
+++ YouVents/YouVents/Pages/Events/Checkout.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.Sqlite;
+using YouVents.API;
 using YouVents.Models;
 
 namespace YouVents.Pages.Events.Purchase
@@ -13,43 +14,15 @@ namespace YouVents.Pages.Events.Purchase
     [Authorize]
     public class CheckoutModel : PageModel
     {
-        public Event MyEvent { get; set; }
+        public Event Event { get; set; }
         public int NumTickets { get; set; }
         public float TotalCost { get; set; }
 
         public IActionResult OnGet(int id, int num_tix)
         {
             NumTickets = num_tix;
-
-            using SqliteConnection connection = new SqliteConnection("Data Source=YouVents.db");
-            using SqliteCommand cmd = new SqliteCommand($"SELECT * FROM Events WHERE Id={id}", connection);
-            connection.Open();
-            using (SqliteDataReader reader = cmd.ExecuteReader())
-            {
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        MyEvent = new Event
-                        {
-                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            OrganizerId = reader.GetString(reader.GetOrdinal("OrganizerId")),
-                            Name = reader.GetString(reader.GetOrdinal("Name")),
-                            Rating = reader.GetInt32(reader.GetOrdinal("Rating")),
-                            Description = reader.GetString(reader.GetOrdinal("Description")),
-                            Date = reader.GetString(reader.GetOrdinal("Date")),
-                            Time = reader.GetString(reader.GetOrdinal("Time")),
-                            Capacity = reader.GetInt32(reader.GetOrdinal("Capacity")),
-                            Street = reader.GetString(reader.GetOrdinal("Street")),
-                            City = reader.GetString(reader.GetOrdinal("City")),
-                            State = reader.GetString(reader.GetOrdinal("State")),
-                            Zip = reader.GetString(reader.GetOrdinal("Zip")),
-                            Price = reader.GetFloat(reader.GetOrdinal("Price"))
-                        };
-                    }
-                }
-            }
-            TotalCost = NumTickets * MyEvent.Price;
+            Event = EventsMethods.GetById(id);
+            TotalCost = NumTickets * Event.Price;
             return Page();
         }
 
