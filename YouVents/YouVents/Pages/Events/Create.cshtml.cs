313d431a5cd09e76214217a89044e2f90a1901f0
TravisBubb <travisbubb14@gmail.com>
2022-04-02 13:47:29 -0400

    Removed queries from backend of page; call API instead

--- YouVents/YouVents/Pages/Events/Create.cshtml.cs
+++ YouVents/YouVents/Pages/Events/Create.cshtml.cs
@@ -8,13 +8,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.Sqlite;
+using YouVents.API;
 using YouVents.Areas.Identity.Data;
 
 namespace YouVents.Pages.Events
 {
     public class CreateModel : PageModel
     {
-        public ApplicationUser user { get; set; }
+        public new ApplicationUser User { get; set; }
 
         [BindProperty]
         public InputModel Input { get; set; }
@@ -58,32 +59,14 @@ namespace YouVents.Pages.Events
         {
             // Verify that the user is an organizer
             string userId = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            User = UsersMethods.GetById(userId);
 
-            using SqliteConnection connection = new SqliteConnection("Data Source=YouVents.db");
-            using SqliteCommand cmd = new SqliteCommand($"SELECT * FROM AspNetUsers WHERE UserName='{userId}'", connection);
-            connection.Open();
-            using (SqliteDataReader reader = cmd.ExecuteReader())
-            {
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        user = new ApplicationUser
-                        {
-                            Id = reader.GetString(reader.GetOrdinal("Id")),
-                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                            DOB = Convert.ToDateTime(reader.GetString(reader.GetOrdinal("DOB"))),
-                            AccountType = reader.GetString(reader.GetOrdinal("AccountType"))
-                        };
-                    }
-                }
-            }
-
-            if (user.AccountType == "Organizer")
+            // If the user is an organizer, then proceed to the creation form
+            if (User.AccountType == "Organizer")
             {
                 return Page();
             }
+            // If the user is not an organizer, then redirect them to the browse page
             else
             {
                 return RedirectToPage("/Events/Index");
