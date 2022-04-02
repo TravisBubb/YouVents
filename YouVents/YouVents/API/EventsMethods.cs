3bed0d4bc3eaa320d5d069318528a72c5d9b8887
TravisBubb <travisbubb14@gmail.com>
2022-04-02 13:00:43 -0400

    Created an API for Events - added GetAll() function

---
+++ YouVents/YouVents/API/EventsMethods.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using YouVents.Models;
+
+namespace YouVents.API
+{
+    // Static class that holds all backend methods dealing with Event objects
+    public static class EventsMethods
+    {
+        // Return a list of all of the Event objects in the database
+        public static List<Event> GetAll()
+        {
+            // Declare a list of Event objects - to be returned
+            List<Event> Events = new List<Event>();
+
+            using SqliteConnection connection = new SqliteConnection("Data Source=YouVents.db");
+            SqliteCommand cmd = new SqliteCommand($"SELECT * FROM Events", connection);
+            connection.Open();
+            using (SqliteDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Event MyEvent = new Event
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            OrganizerId = reader.GetString(reader.GetOrdinal("OrganizerId")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Rating = reader.GetInt32(reader.GetOrdinal("Rating")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            Date = reader.GetString(reader.GetOrdinal("Date")),
+                            Time = reader.GetString(reader.GetOrdinal("Time")),
+                            Capacity = reader.GetInt32(reader.GetOrdinal("Capacity")),
+                            Street = reader.GetString(reader.GetOrdinal("Street")),
+                            City = reader.GetString(reader.GetOrdinal("City")),
+                            State = reader.GetString(reader.GetOrdinal("State")),
+                            Zip = reader.GetString(reader.GetOrdinal("Zip")),
+                            Price = reader.GetFloat(reader.GetOrdinal("Price"))
+                        };
+                        Events.Add(MyEvent);
+                    }
+                }
+            }
+
+            return Events;
+        }
+    }
+}
