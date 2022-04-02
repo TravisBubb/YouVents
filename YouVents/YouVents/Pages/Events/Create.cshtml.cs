2f60be88e18f6c6fc2f9981897adfd81227df1cd
TravisBubb <travisbubb14@gmail.com>
2022-04-02 15:07:14 -0400

    Updated to call API function to create event; also changed time and date format strings

--- YouVents/YouVents/Pages/Events/Create.cshtml.cs
+++ YouVents/YouVents/Pages/Events/Create.cshtml.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.Sqlite;
 using YouVents.API;
 using YouVents.Areas.Identity.Data;
+using YouVents.Models;
 
 namespace YouVents.Pages.Events
 {
@@ -20,6 +21,7 @@ namespace YouVents.Pages.Events
         [BindProperty]
         public InputModel Input { get; set; }
 
+        // Class to hold all of the inputs in the form
         public class InputModel
         {
             [Required]
@@ -52,9 +54,12 @@ namespace YouVents.Pages.Events
             
             [Required]
             public float Price { get; set; }
+
+            [Required]
+            public int Rating { get; set; }
         }
 
-
+        // On a Get action, simply load the page if the user is an organizer
         public IActionResult OnGet()
         {
             // Verify that the user is an organizer
@@ -73,8 +78,32 @@ namespace YouVents.Pages.Events
             }
         }
 
+        // On a Post action, create a new Event and attempt to insert it into the database
         public IActionResult OnPost()
         {
+            // Get the user's Id string
+            string userId = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+
+            // Create a new Event to insert based on the inputs in the form
+            Event e = new Event
+            {
+                Name = Input.Name,
+                Rating = Input.Rating,
+                Description = Input.Description,
+                Date = Input.Date.ToString("ddd, dd MMM yyyy"),
+                Time = Input.Time.ToString("hh:mm tt"),
+                Capacity = Input.Capacity,
+                Street = Input.Street,
+                City = Input.City,
+                State = Input.State,
+                OrganizerId = userId,
+                Zip = Input.Zip,
+                Price = Input.Price
+            };
+
+            // Try to insert the event into the database
+            EventsMethods.Create(e);
+
             return RedirectToPage("/Events/CreationConfirmation");
         }
     }
