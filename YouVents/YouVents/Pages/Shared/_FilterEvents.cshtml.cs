9b5a305fa3ba839da196d2b28c56723864e4f70f
TravisBubb <travisbubb14@gmail.com>
2022-04-04 19:01:39 -0400

    Added responsive search bar for events list; only added for fans

---
+++ YouVents/YouVents/Pages/Shared/_FilterEvents.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YouVents.Pages.Shared
+{
+    public class FilterEventsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
