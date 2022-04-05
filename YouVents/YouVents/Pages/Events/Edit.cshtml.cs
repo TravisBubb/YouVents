e0edaf188f150aa472967e6c25bbaec4e5bb5278
TravisBubb <travisbubb14@gmail.com>
2022-04-05 15:17:30 -0400

    Display all organizer's events; Click on event to go to its edit page

---
+++ YouVents/YouVents/Pages/Events/Edit.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YouVents.API;
+using YouVents.Models;
+
+namespace YouVents.Pages.Events
+{
+    public class EditModel : PageModel
+    {
+        public Event Event { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Event = EventsMethods.GetById(id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
--- YouVents/YouVents/Pages/Events/Manage.cshtml.cs
+++ YouVents/YouVents/Pages/Events/Manage.cshtml.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using YouVents.API;
+using YouVents.Areas.Identity.Data;
+using YouVents.Models;
 
 namespace YouVents.Pages.Events
 {
     public class ManageModel : PageModel
     {
-        public void OnGet()
-        {
+        public new ApplicationUser User { get; set; }
+        public List<Event> Events { get; set; }
 
+
+        public IActionResult OnGet()
+        {
+            string userId = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            Events = EventsMethods.GetAllByOrgId(userId);
+
+            return Page();
         }
     }
 }
