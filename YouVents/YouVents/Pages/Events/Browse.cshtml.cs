6b5fbda753781376ab1dafad7e46e1afd506f45c
TravisBubb <travisbubb14@gmail.com>
2022-04-05 14:19:49 -0400

    Added universal browse page; Organizer has dedicated Manage page (unfinished)

---
+++ YouVents/YouVents/Pages/Events/Browse.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YouVents.API;
+using YouVents.Areas.Identity.Data;
+using YouVents.Models;
+
+namespace YouVents.Pages.Events
+{
+    public class BrowseModel : PageModel
+    {
+        public List<Event> Events { get; set; }
+        public int NumTickets { get; set; }
+        public new ApplicationUser User { get; set; }
+
+        public IActionResult OnGet()
+        {
+            Events = EventsMethods.GetAllFuture();
+
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                string userId = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+                User = UsersMethods.GetById(userId);
+            }
+            return Page();
+        }
+    }
+}
--- YouVents/YouVents/Pages/Events/Index.cshtml.cs
+++ YouVents/YouVents/Pages/Events/Index.cshtml.cs
@@ -10,20 +10,20 @@ namespace YouVents.Pages.Events
 {
     public class IndexModel : PageModel
     {
-        public List<Event> Events { get; set; }
-        public int NumTickets { get; set; }
-        public new ApplicationUser User { get; set; }
+        //public List<Event> Events { get; set; }
+        //public int NumTickets { get; set; }
+        //public new ApplicationUser User { get; set; }
 
-        public IActionResult OnGet()
-        {
-            Events = EventsMethods.GetAllFuture();
+        //public IActionResult OnGet()
+        //{
+        //    Events = EventsMethods.GetAllFuture();
          
-            if (HttpContext.User.Identity.IsAuthenticated)
-            {
-                string userId = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
-                User = UsersMethods.GetById(userId);
-            }
-            return Page();
-        }
+        //    if (HttpContext.User.Identity.IsAuthenticated)
+        //    {
+        //        string userId = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+        //        User = UsersMethods.GetById(userId);
+        //    }
+        //    return Page();
+        //}
     }
 }
