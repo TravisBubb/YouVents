b483a1fcb460e922b4a444f66b08f2389effcdcd
TravisBubb <travisbubb14@gmail.com>
2022-04-05 15:17:57 -0400

    Added method to get all events given organizer ID

--- YouVents/YouVents/API/EventsMethods.cs
+++ YouVents/YouVents/API/EventsMethods.cs
@@ -233,5 +233,51 @@ namespace YouVents.API
 
             return Types;
         }
+
+        // Return all events (old and future) given a specific organizer's Id
+        public static List<Event> GetAllByOrgId(string orgId)
+        {
+            // Declare a list of Event objects - to be returned
+            List<Event> Events = new List<Event>();
+
+            using SqliteConnection connection = new SqliteConnection("Data Source=YouVents.db");
+            SqliteCommand cmd = new SqliteCommand($"SELECT * FROM Events WHERE OrganizerId=@orgId", connection);
+            cmd.Parameters.Add(new SqliteParameter("@orgId", orgId));
+            connection.Open();
+            using (SqliteDataReader reader = cmd.ExecuteReader())
+            {
+                // Check if there are any rows to read, given the above query
+                if (reader.HasRows)
+                {
+                    // Loop through each row in the query result
+                    while (reader.Read())
+                    {
+                        // Create an event object and add it to the list
+                        Event MyEvent = new Event
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            OrganizerId = reader.GetString(reader.GetOrdinal("OrganizerId")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Rating = reader.GetInt32(reader.GetOrdinal("Rating")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            Date = reader.GetString(reader.GetOrdinal("Date")),
+                            Time = reader.GetString(reader.GetOrdinal("Time")),
+                            Capacity = reader.GetInt32(reader.GetOrdinal("Capacity")),
+                            Street = reader.GetString(reader.GetOrdinal("Street")),
+                            City = reader.GetString(reader.GetOrdinal("City")),
+                            State = reader.GetString(reader.GetOrdinal("State")),
+                            Zip = reader.GetString(reader.GetOrdinal("Zip")),
+                            Price = reader.GetFloat(reader.GetOrdinal("Price"))
+                        };
+                        Events.Add(MyEvent);
+                    }
+                }
+            }
+            // Sort the list of events by date and time in ascending order
+            SortByDate(Events);
+
+            // Return the list of events that were returned -- all events in the db
+            return Events;
+        }
     }
 }
