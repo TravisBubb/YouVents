274c2a8ee3922b8729a6cab7ab9fb87cbb89d6e9
Mushkela <andrewgendy9@gmail.com>
2022-04-06 01:16:14 -0400

    Added gitignore to ignore all bin/ category

--- YouVents/YouVents/Pages/Events/Browse.cshtml.cs
+++ YouVents/YouVents/Pages/Events/Browse.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,6 +15,7 @@ namespace YouVents.Pages.Events
         public List<Event> Events { get; set; }
         public int NumTickets { get; set; }
         public new ApplicationUser User { get; set; }
+        public InputModel Input { get; set; }
 
         public IActionResult OnGet()
         {
@@ -25,5 +28,56 @@ namespace YouVents.Pages.Events
             }
             return Page();
         }
+        public class InputModel {
+
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            [DataType(DataType.Date)]
+            public DateTime Date { get; set; }
+
+            [DataType(DataType.Time)]
+            public DateTime Time { get; set; }
+
+            public int Capacity { get; set; }
+
+            public string Street { get; set; }
+
+            public string City { get; set; }
+
+            public string State { get; set; }
+
+            public string Zip { get; set; }
+
+            public float Price { get; set; }
+
+            public int Rating { get; set; }
+
+            public string Type { get; set; }
+        }
+        //Testing EventsQuery-->
+        public IActionResult OnPost() {
+
+            Event e = new Event {
+                Name = Input.Name,
+                Rating = Input.Rating,
+                Description = Input.Description,
+                Date = Input.Date.ToString("ddd, dd MMM yyyy"),
+                Time = Input.Time.ToString("hh:mm tt"),
+                Capacity = Input.Capacity,
+                Street = Input.Street,
+                City = Input.City,
+                State = Input.State,
+                Zip = Input.Zip,
+                Price = Input.Price,
+                Type = Input.Type
+            };
+
+            EventsMethods.EventsQuery(e);
+            return Page();
+        
+        }
+        //end of EventsQuery Testing-->
     }
 }
--- YouVents/YouVents/wwwroot/js/site.js
+++ YouVents/YouVents/wwwroot/js/site.js
@@ -1,4 +1,93 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Filter the list of Events based on input in the search bar
+$(document).ready(function () {
 
-// Write your Javascript code.
+    // Try to perform a filter after a key-up event in the search bar
+    $("#EventSearch").on("keyup", function () {
+
+        // Get the text that is present in the search bar
+        var value = $(this).val().toLowerCase();
+
+        // Attempt to filter the Events list based on the title of the events
+        $("#EventList a").filter(function () {
+
+            // Normalize the text to lowercase and split into an array of lines
+            var lines = $(this).text().toLowerCase().trim().split("\n");
+
+            // Get the title line
+            var eventName = lines[0];
+
+            // Only show the Events whose names contain the search bar contents
+            $(this).toggle(eventName.indexOf(value) > -1);
+        });
+    });
+    // Try to perform a filter after a key-up event in the City bar
+    //$("#FilterBtn").on('click', function () {
+
+    //    // Get the text that is present in the search bar
+    //    var value = $(this).val().toLowerCase(); //keyword "this" doesn't work anymore because I changed the "#CitySearch" to "#FilterBtn"
+
+    //    // Attempt to filter the Events list based on the title of the events
+    //    $("#EventList a").filter(function () {
+
+    //        // Normalize the text to lowercase and split into an array of lines
+    //        var lines = $(this).text().toLowerCase().trim().split("\n");
+
+
+    //        // Get the title line
+    //        var cityName = lines[4].trim().split(": ")[1];
+
+    //        // Only show the Events whose names contain the search bar contents
+    //        $(this).toggle(cityName.indexOf(value) > -1);
+    //    });
+    //});
+});
+
+////var date = $("#DateSearch").val().split("/");
+
+
+
+//function NameFilter() {
+//    // Get the text that is present in the search bar
+//    var value = $(this).val().toLowerCase();
+
+//    // Attempt to filter the Events list based on the title of the events
+//    $("#EventList a").filter(function () {
+
+//        // Normalize the text to lowercase and split into an array of lines
+//        var lines = $(this).text().toLowerCase().trim().split("\n");
+
+//        // Get the title line
+//        var eventName = lines[0];
+
+//        // Only show the Events whose names contain the search bar contents
+//        $(this).toggle(eventName.indexOf(value) > -1);
+//    });
+//}
+
+//function CityFilter() {
+//    // Get the text that is present in the search bar
+//    var value = $(this).val().toLowerCase();
+
+//    // Attempt to filter the Events list based on the title of the events
+//    $("#EventList a").filter(function () {
+
+//        // Normalize the text to lowercase and split into an array of lines
+//        var lines = $(this).text().toLowerCase().trim().split("\n");
+
+
+//        // Get the title line
+//        var cityName = lines[4].trim().split(": ")[1];
+
+//        // Only show the Events whose names contain the search bar contents
+//        $(this).toggle(cityName.indexOf(value) > -1);
+//    });
+//}
+
+//$(document).ready(function () {
+//    // Try to perform a filter after a key-up event in the search bar
+//    $("#EventSearch").on("keyup", NameFilter());
+//    // Try to perform a filter after a key-up event in the city search bar
+//    $("#CitySearch").on("keyup", CityFilter());
+//    )};
+
+
