24f051a97409e5d7a97d996e207ab85d6c4bfbe3
Mushkela <andrewgendy9@gmail.com>
2022-04-07 23:35:41 -0400

    Fixed layout problem
    Started using Bootstrap 4 instead of 5 and that seems to have fixed the issue.
    The link to bs4 is added to Pages/Shared/_Layout.cshtml

--- YouVents/YouVents/wwwroot/js/site.js
+++ YouVents/YouVents/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Filter the list of Events based on input in the search bar
+﻿ //Filter the list of Events based on input in the search bar
 $(document).ready(function () {
 
     // Try to perform a filter after a key-up event in the search bar
@@ -33,53 +33,6 @@ $(document).ready(function () {
     });
 });
 
-////var date = $("#DateSearch").val().split("/");
-
-
-
-//function NameFilter() {
-//    // Get the text that is present in the search bar
-//    var value = $(this).val().toLowerCase();
-
-//    // Attempt to filter the Events list based on the title of the events
-//    $("#EventList a").filter(function () {
-
-//        // Normalize the text to lowercase and split into an array of lines
-//        var lines = $(this).text().toLowerCase().trim().split("\n");
-
-//        // Get the title line
-//        var eventName = lines[0];
-
-//        // Only show the Events whose names contain the search bar contents
-//        $(this).toggle(eventName.indexOf(value) > -1);
-//    });
-//}
-
-//function CityFilter() {
-//    // Get the text that is present in the search bar
-//    var value = $(this).val().toLowerCase();
-
-//    // Attempt to filter the Events list based on the title of the events
-//    $("#EventList a").filter(function () {
-
-//        // Normalize the text to lowercase and split into an array of lines
-//        var lines = $(this).text().toLowerCase().trim().split("\n");
-
-
-//        // Get the title line
-//        var cityName = lines[4].trim().split(": ")[1];
-
-//        // Only show the Events whose names contain the search bar contents
-//        $(this).toggle(cityName.indexOf(value) > -1);
-//    });
-//}
-
-//$(document).ready(function () {
-//    // Try to perform a filter after a key-up event in the search bar
-//    $("#EventSearch").on("keyup", NameFilter());
-//    // Try to perform a filter after a key-up event in the city search bar
-//    $("#CitySearch").on("keyup", CityFilter());
-//    )};
 
 
 function SortByPrice() {
