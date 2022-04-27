baa220daf292083f00d26cedd030cbd60d36f8eb
TravisBubb <travisbubb14@gmail.com>
2022-04-26 21:13:29 -0400

    Created a new landing page for the site called TestCover--not finished; Started re-working browse page to have filter menu on left side

--- YouVents/YouVents/wwwroot/js/site.js
+++ YouVents/YouVents/wwwroot/js/site.js
@@ -8,13 +8,15 @@ $(document).ready(function () {
         var value = $(this).val().toLowerCase();
 
         // Attempt to filter the Events list based on the title of the events
-        $("#EventList a").filter(function () {
+        $("#EventList span").filter(function () {
 
             // Normalize the text to lowercase and split into an array of lines
             var lines = $(this).text().toLowerCase().trim().split("\n");
 
             // Get the title line
             var eventName = lines[0];
+
+            console.log(lines);
 
             // Only show the Events whose names contain the search bar contents
             $(this).toggle(eventName.indexOf(value) > -1);
